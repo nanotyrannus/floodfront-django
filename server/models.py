aeaf362e86b9c11ec772b501fd9260f23693e96d
Ryan Constantino <ryan.constantino93@gmail.com>
2020-12-04 19:52:00 -0500

    DateField to DateTimeField for increased granularity.

--- server/models.py
+++ server/models.py
@@ -8,7 +8,7 @@ class Marker(models.Model):
     is_directional = models.BooleanField(default=False)
     accuracy = models.FloatField(default=-1)
     description = models.TextField(default="")
-    created_on = models.DateField(auto_now=True) # updated whenever instance is modified
+    created_on = models.DateTimeField(auto_now=True) # updated whenever instance is modified
     marker_type = models.TextField(null=False)
     email = models.EmailField(null=False)
 
